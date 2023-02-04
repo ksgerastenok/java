a4dac0c24a905504fc6e32416db8189486b03008
Konstantin Gerastenok <frankgover@rambler.ru>
2023-02-05 00:21:45 +0300

    Add files via upload

---
+++ Task/AOptional.java
@@ -0,0 +1,170 @@
+package event;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public abstract class AOptional<T> implements Supplier<T> {
+    public static <T> AOptional<T> of(T value) {
+        return new AOptional<>() {
+            public T get() {
+                T tmp = value;
+                if (Objects.nonNull(tmp)) {
+                    return tmp;
+                }
+                throw new NullPointerException();
+            }
+        };
+    }
+
+    public static <T> AOptional<T> ofNullable(T value) {
+        return new AOptional<>() {
+            public T get() {
+                T tmp = value;
+                if (Objects.nonNull(tmp)) {
+                    return tmp;
+                }
+                return null;
+            }
+        };
+    }
+
+    public AOptional<T> or(AOptional<T> other) {
+        return new AOptional<>() {
+            public T get() {
+                T tmp = AOptional.this.get();
+                if (Objects.nonNull(tmp)) {
+                    return tmp;
+                }
+                if (Objects.nonNull(other)) {
+                    return other.get();
+                }
+                return null;
+            }
+        };
+    }
+
+    public AOptional<T> filter(Function<T, Boolean> function) {
+        return new AOptional<>() {
+            public T get() {
+                T tmp = AOptional.this.get();
+                if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                    if (function.apply(tmp)) {
+                        return tmp;
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
+    public <U> AOptional<U> flatMap(Function<T, AOptional<U>> function) {
+        return new AOptional<>() {
+            public U get() {
+                T tmp = AOptional.this.get();
+                if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                    return function.apply(tmp).get();
+                }
+                return null;
+            }
+        };
+    }
+
+    public <U> AOptional<U> map(Function<T, U> function) {
+        return new AOptional<>() {
+            public U get() {
+                T tmp = AOptional.this.get();
+                if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                    return function.apply(tmp);
+                }
+                return null;
+            }
+        };
+    }
+
+    public T orElse(T other) {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        if (Objects.nonNull(other)) {
+            return other;
+        }
+        return null;
+    }
+
+    public T orElseThrow() {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        throw new NullPointerException();
+    }
+
+    public <U extends Throwable> T orElseThrow(U throwable) throws U {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        if (Objects.nonNull(throwable)) {
+            throw throwable;
+        }
+        throw new NullPointerException();
+    }
+
+    public boolean isEmpty() {
+        T tmp = AOptional.this.get();
+        return Objects.isNull(tmp);
+    }
+
+    public boolean isPresent() {
+        T tmp = AOptional.this.get();
+        return Objects.nonNull(tmp);
+    }
+
+    public void ifPresent(Consumer<T> consumer) {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp) && Objects.nonNull(consumer)) {
+            consumer.accept(tmp);
+        }
+    }
+
+    public void ifPresent(Consumer<T> consumer, Runnable runnable) {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp) && Objects.nonNull(consumer)) {
+            consumer.accept(tmp);
+        }
+        if (Objects.nonNull(runnable)) {
+            runnable.run();
+        }
+    }
+
+    public int hashCode() {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp)) {
+            return tmp.hashCode();
+        }
+        return 0;
+    }
+
+    public String toString() {
+        T tmp = AOptional.this.get();
+        if (Objects.nonNull(tmp)) {
+            return String.format("AOptional[%s]", tmp);
+        }
+        return String.format("AOptional[%s]", "empty");
+    }
+
+    public boolean equals(Object other) {
+        return AOptional.ofNullable(other)
+                .filter(AOptional.class::isInstance)
+                .map(AOptional.class::cast)
+                .filter(AOptional.this::equals)
+                .isPresent();
+    }
+
+    public <U> boolean equals(AOptional<U> other) {
+        return Objects.equals(other.get(), AOptional.this.get());
+    }
+}
---
+++ Task/COptional.java
@@ -0,0 +1,168 @@
+package event;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+final class COptional<T> {
+    private final Supplier<T> value;
+
+    private COptional(Supplier<T> value) {
+        this.value = value;
+    }
+
+    private T getValue() {
+        return this.value.get();
+    }
+
+    public static <T> COptional<T> of(T value) {
+        return new COptional<>(() -> {
+            T tmp = value;
+            if (Objects.nonNull(tmp)) {
+                return tmp;
+            }
+            throw new NullPointerException();
+        });
+    }
+
+    public static <T> COptional<T> ofNullable(T value) {
+        return new COptional<>(() -> {
+            T tmp = value;
+            if (Objects.nonNull(tmp)) {
+                return tmp;
+            }
+            return null;
+        });
+    }
+
+    public COptional<T> or(COptional<T> other) {
+        return new COptional<>(() -> {
+            T tmp = this.getValue();
+            if (Objects.nonNull(tmp)) {
+                return tmp;
+            }
+            if (Objects.nonNull(other)) {
+                return other.getValue();
+            }
+            return null;
+        });
+    }
+
+    public COptional<T> filter(Function<T, Boolean> function) {
+        return new COptional<>(() -> {
+            T tmp = this.getValue();
+            if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                if (function.apply(tmp)) {
+                    return tmp;
+                }
+            }
+            return null;
+        });
+    }
+
+    public <U> COptional<U> flatMap(Function<T, COptional<U>> function) {
+        return new COptional<>(() -> {
+            T tmp = this.getValue();
+            if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                return function.apply(tmp).getValue();
+            }
+            return null;
+        });
+    }
+
+    public <U> COptional<U> map(Function<T, U> function) {
+        return new COptional<>(() -> {
+            T tmp = this.getValue();
+            if (Objects.nonNull(tmp) && Objects.nonNull(function)) {
+                return function.apply(tmp);
+            }
+            return null;
+        });
+    }
+
+    public T orElse(T other) {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        if (Objects.nonNull(other)) {
+            return other;
+        }
+        return null;
+    }
+
+    public T orElseThrow() {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        throw new NullPointerException();
+    }
+
+    public <U extends Throwable> T orElseThrow(U throwable) throws U {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp)) {
+            return tmp;
+        }
+        if (Objects.nonNull(throwable)) {
+            throw throwable;
+        }
+        throw new NullPointerException();
+    }
+
+    public boolean isEmpty() {
+        T tmp = this.getValue();
+        return Objects.isNull(tmp);
+    }
+
+    public boolean isPresent() {
+        T tmp = this.getValue();
+        return Objects.nonNull(tmp);
+    }
+
+    public void ifPresent(Consumer<T> consumer) {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp) && Objects.nonNull(consumer)) {
+            consumer.accept(tmp);
+        }
+    }
+
+    public void ifPresent(Consumer<T> consumer, Runnable runnable) {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp) && Objects.nonNull(consumer)) {
+            consumer.accept(tmp);
+        }
+        if (Objects.nonNull(runnable)) {
+            runnable.run();
+        }
+    }
+
+    public int hashCode() {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp)) {
+            return tmp.hashCode();
+        }
+        return 0;
+    }
+
+    public String toString() {
+        T tmp = this.getValue();
+        if (Objects.nonNull(tmp)) {
+            return String.format("COptional[%s]", tmp);
+        }
+        return String.format("COptional[%s]", "empty");
+    }
+
+    public boolean equals(Object other) {
+        return COptional.ofNullable(other)
+                .filter(COptional.class::isInstance)
+                .map(COptional.class::cast)
+                .filter(COptional.this::equals)
+                .isPresent();
+    }
+
+    public <U> boolean equals(COptional<U> other) {
+        return Objects.equals(other.getValue(), this.getValue());
+    }
+}
