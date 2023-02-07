7ad0031cf1cb311c05f219d1683f30a9c97f3f7c
Konstantin Gerastenok <frankgover@rambler.ru>
2023-02-07 22:10:03 +0300

    Update calendar.sql

--- Task/calendar.sql
+++ Task/calendar.sql
@@ -1,11 +1,11 @@
 SELECT
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 1, 'DD.MM.YYYY') AS "MON",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 2, 'DD.MM.YYYY') AS "TUE",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 3, 'DD.MM.YYYY') AS "WED",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 4, 'DD.MM.YYYY') AS "THU",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 5, 'DD.MM.YYYY') AS "FRI",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 6, 'DD.MM.YYYY') AS "SAT",
-  to_char(trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 7, 'DD.MM.YYYY') AS "SUN"
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 1 AS "MON",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 2 AS "TUE",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 3 AS "WED",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 4 AS "THU",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 5 AS "FRI",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 6 AS "SAT",
+  trunc(sysdate, 'MM') - to_number(to_char(trunc(sysdate, 'MM'), 'D')) + 7 * (LEVEL - 1) + 7 AS "SUN"
 FROM
   sys.dual
 CONNECT BY
