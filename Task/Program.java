53fef69eb72c91991956090a471f1ba674a6c61e
Konstantin Gerastenok <frankgover@rambler.ru>
2023-11-13 16:07:00 +0300

    Update Program.java

--- Task/Program.java
+++ Task/Program.java
@@ -19,6 +19,23 @@ public class Program {
         return result.toString();
     }
 
+    public static void deleteSpaces(char[] data, char delimiter) {
+        int count = 0;
+
+        for (int i = 0; i != data.length; i += 1) {
+            if (data[i] != delimiter) {
+                char tmp;
+                tmp = data[i - count];
+                data[i - count] = data[i];
+                data[i] = tmp;
+            } else {
+                if (((i - count - 1 < 0) || (data[i - count - 1] == delimiter))) {
+                    count += 1;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(deleteSpaces("___On__my___home_world___", '_'));
     }
