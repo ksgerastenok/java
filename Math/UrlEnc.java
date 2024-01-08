07f9c5500d49cf4e43d2bdd7358c7f13a3e613d9
Konstantin Gerastenok <frankgover@rambler.ru>
2024-01-08 22:40:24 +0300

    Update UrlEnc.java

--- Math/UrlEnc.java
+++ Math/UrlEnc.java
@@ -2,34 +2,35 @@ package event;
 
 
 public class Program extends Object {
-    public static void replace(char[] data) {
+    public static int replace(char[] data, int length) {
         int count = 0;
-
-        for (int i = 0; i != data.length; i += 1) {
-            if (data[i] == ' ') {
+        for (int i = length; i != 0; i -= 1) {
+            if (data[i - 1] == ' ') {
                 count += 1;
             }
         }
 
-        for (int i = data.length; i != 0; i -= 1) {
-            if (data[i - count * 2 - 1] == ' ') {
-                data[i - count * 2 - 1] = '%';
-                data[i - count * 2] = '2';
-                data[i - count * 2 + 1] = '0';
+        int result = length + count * 2;
+        for (int i = length; i != 0; i -= 1) {
+            if (data[i - 1] == ' ') {
+                data[i - 1] = '%';
+                data[i] = '2';
+                data[i + 1] = '0';
                 count -= 1;
+                data[i - 1 + count * 2] = '%';
+                data[i + count * 2] = '2';
+                data[i + 1 + count * 2] = '0';
             }
-
-            data[i - 1] = data[i - count * 2 - 1];
+            data[i - 1 + count * 2] = data[i - 1];
         }
 
-        return;
+        return result;
     }
 
-    public static void print(char[] data) {
-        for (int i = 0; i != data.length; i += 1) {
+    public static void print(char[] data, int length) {
+        for (int i = 0; i != length; i += 1) {
             System.out.print(data[i]);
         }
-
         System.out.println();
 
         return;
