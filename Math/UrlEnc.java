d3bd13b12f7ffa8a4cdfa3a4c724388d328d88f0
Konstantin Gerastenok <frankgover@rambler.ru>
2024-02-13 13:32:59 +0300

    Update UrlEnc.java

--- Math/UrlEnc.java
+++ Math/UrlEnc.java
@@ -13,15 +13,13 @@ public class Program extends Object {
         int result = length + count * 2;
         for (int i = length; i != 0; i -= 1) {
             if (data[i - 1] == ' ') {
-                data[i - 1] = '%';
-                data[i] = '2';
-                data[i + 1] = '0';
                 count -= 1;
                 data[i - 1 + count * 2] = '%';
                 data[i + count * 2] = '2';
                 data[i + 1 + count * 2] = '0';
+            } else {
+                data[i - 1 + count * 2] = data[i - 1];
             }
-            data[i - 1 + count * 2] = data[i - 1];
         }
 
         return result;
@@ -37,9 +35,9 @@ public class Program extends Object {
     }
 
     public static void main(String[] args) {
-        char[] data = new char[] { 'm', ' ', 'y', ' ', 'd', ' ', 'a', ' ', 't', 'a', 0, 0, 0, 0, 0, 0, 0, 0 };
+        char[] data = new char[] { 'm', ' ', 'y', ' ', 'd', ' ', 'a', ' ', ' ', 't', 'a', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-        print(data, replace(data, 10));
+        print(data, replace(data, 11));
 
         return;
     }
